6c81faaac0d11134560316072244bf36aa845f80
Berkan1 <berkan.c@hotmail.com>
2023-06-11 16:59:06 +0100

    Changed CV file

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -6,7 +6,7 @@ function AboutMe() {
       <Container>
         <img src={process.env.PUBLIC_URL + '/profile-pic.jpg'} alt='Berkan' className='profile'/>
         <article>
-          <p>Hi there! I'm <span>Berkan</span>, a <span>frontend developer</span> from London with over <span>3 
+          <p>Hi there! I'm <span>Berkan</span>, a <span>frontend developer</span> from London with over <span>5 
             years</span> of professional experience creating interactive web applications.
           </p><br/>
           <p>I'm a big fan of using tools like <span>React</span> to build responsive, accessible and performant software that can make a positive social impact!
